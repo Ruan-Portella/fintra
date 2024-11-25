597df9bef56cc215cb3a17480e726a93aca76a4e
Ruan Portella <ruanmorales29@gmail.com>
2024-11-25 20:09:54 -0300

    Feat: Cria endpoint summary

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { eachDayOfInterval, isSameDay } from "date-fns"
 import { NextResponse } from "next/server"
 import { twMerge } from "tailwind-merge"
 
@@ -8,4 +9,47 @@ export function cn(...inputs: ClassValue[]) {
 
 export function sendError(errors: { error: string, status: number }) {
   return NextResponse.json({ error: errors.error }, { status: errors.status }) 
+}
+
+export function calculatePercentageChange(
+  current: number,
+  previous: number
+) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100
+  }
+
+  return ((current - previous) / Math.abs(previous)) * 100
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date,
+    income: number,
+    expenses: number
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return []
+  }
+
+  const allDays = eachDayOfInterval({ start: startDate, end: endDate });
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if (found) {
+      return found
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0
+      }
+    }
+  });
+
+  return transactionsByDay
 }
